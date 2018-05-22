a6851c3b2d0f3abb99bb7c2b5fad6fb714682c66
Omar Quazi <omar@quazi.co>
2018-05-21 23:17:00 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,78 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, Events } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+
+import { ApiProvider } from '../providers/api/api';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = 'HomePage';
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  stories: any = [];
+
+  constructor(platform: Platform, statusBar: StatusBar,
+    splashScreen: SplashScreen, private localNotifications: LocalNotifications,
+    public api: ApiProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.localNotificationConfig();
+
+      this.getTopStories();
+
+
     });
   }
-}
 
+
+  localNotificationConfig() {
+    this.localNotifications.setDefaults({
+      led: { color: '#FF00FF', on: 1000, off: 1000 },
+      vibrate: false
+    });
+  }
+
+  startNotifications(id) {
+
+
+    //let randomStory = Math.floor((Math.random() * this.stories.length) + 1);
+
+    this.localNotifications.schedule({
+      id: id,
+      title: this.stories[0],
+      text: this.stories[0],
+      every: 'minute',
+      sound: null,
+      data: { id: 1, title: "HEADLINE" }
+    });
+
+    this.stories.splice(0, 1);
+
+  }
+
+  getTopStories() {
+    let key = '4b4d9e491bba4b428219d908b7eb94fc';
+
+    var api = this.api;
+
+    api.get('home.json').subscribe(res => {
+      this.stories = res.json()['results'];
+      this.startNotifications(1);
+    }, err => {
+
+      console.log(err);
+
+    });
+
+
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,34 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { LocalNotifications } from '@ionic-native/local-notifications';
+
+import { ApiProvider } from '../providers/api/api';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ApiProvider,
+    LocalNotifications
   ]
 })
 export class AppModule {}
